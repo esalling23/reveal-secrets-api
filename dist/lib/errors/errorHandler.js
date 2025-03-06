170011ae5696906b88719d53b169cfdf71fec64f
Eron Salling <esalling23@gmail.com>
2025-03-06 14:46:02 -0500

    Builds changes

--- dist/lib/errors/errorHandler.js
+++ dist/lib/errors/errorHandler.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const customErrors_1 = require("./customErrors");
-const errorHandler = (error, _req, res) => {
+const errorHandler = (error, _req, res, _next) => {
     let errorStatus = 500;
     if (error instanceof customErrors_1.MissingSecretError) {
         errorStatus = 400;
