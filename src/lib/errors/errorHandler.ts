c95c2aa07a9cc86534a763dbb82674ff2b831c55
Eron Salling <esalling23@gmail.com>
2025-03-06 14:45:44 -0500

    Adds unused _next callback to errorHandler

--- src/lib/errors/errorHandler.ts
+++ src/lib/errors/errorHandler.ts
@@ -1,7 +1,7 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { MissingSecretError } from "./customErrors";
 
-const errorHandler = (error: Error, _req: Request, res: Response) => {
+const errorHandler = (error: Error, _req: Request, res: Response, _next: NextFunction) => {
   let errorStatus = 500
 
   if (error instanceof MissingSecretError) {
